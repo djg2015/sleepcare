1a9f1a09215828fb887c75806834d8a672accaf8
djg2015 <434914655@qq.com>
2015-10-13 13:52:28 +0800

    11

---
+++ ewell.sleepcareforpad/SleepcareMainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SleepcareMainViewModel.swift
+//  ewell.sleepcareforpad
+//
+//  Created by djg on 15/10/13.
+//  Copyright (c) 2015年 djg. All rights reserved.
+//
+
+import UIKit
+
+class SleepcareMainViewModel: NSObject {
+   
+}
