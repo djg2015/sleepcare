25913cdd9ef264ad146796866d68353ae56dd5d6
djg2015 <434914655@qq.com>
2015-10-13 16:57:47 +0800

    11

--- ewell.sleepcareforpad/SleepcareMainController.swift
+++ ewell.sleepcareforpad/SleepcareMainController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class SleepcareMainController: BaseViewController,UIScrollViewDelegate {
     //界面控件
-   
+    
     @IBOutlet weak var curPager: Pager!
     @IBOutlet weak var search: UISearchBar!
     //类字段
-     var mainScroll:UIScrollView!
+    var mainScroll:UIScrollView!
+    var sleepcareMainViewModel:SleepcareMainViewModel?
+    var BedViews:Array<BedModel>?{
+        didSet{
+            
+            self.mainScroll.contentSize = CGSize(width: self.view.bounds.size.width * 2, height: 500)
+            
+            let mainview1 = NSBundle.mainBundle().loadNibNamed("SleepCareCollectionView", owner: self, options: nil).last as! SleepCareCollectionView
+            mainview1.frame = CGRectMake(0, 0, 1024, self.mainScroll.frame.size.height)
+            //self.mainview = SleepCareCollectionView(frame: CGRectMake(0, 0, 300, 400), collectionViewLayout: UICollectionViewLayout())
+            mainview1.reloadData(TestDate())
+            self.mainScroll.addSubview(mainview1)
+            self.mainScroll.bringSubviewToFront(mainview1)
+            
+            let mainview2 = NSBundle.mainBundle().loadNibNamed("SleepCareCollectionView", owner: self, options: nil).last as! SleepCareCollectionView
+            mainview2.frame = CGRectMake(1024, 0, 1024, self.mainScroll.frame.size.height)
+            //self.mainview = SleepCareCollectionView(frame: CGRectMake(0, 0, 300, 400), collectionViewLayout: UICollectionViewLayout())
+            mainview2.reloadData()
+            self.mainScroll.addSubview(mainview2)
+            self.mainScroll.bringSubviewToFront(mainview2)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,24 +52,9 @@ class SleepcareMainController: BaseViewController,UIScrollViewDelegate {
         self.mainScroll.bounces = false
         self.view.addSubview(self.mainScroll)
         
-        self.mainScroll.contentSize = CGSize(width: self.view.bounds.size.width * 2, height: 500)
-        
-        let mainview1 = NSBundle.mainBundle().loadNibNamed("SleepCareCollectionView", owner: self, options: nil).last as! SleepCareCollectionView
-        mainview1.frame = CGRectMake(0, 0, 1024, self.mainScroll.frame.size.height)
-        //self.mainview = SleepCareCollectionView(frame: CGRectMake(0, 0, 300, 400), collectionViewLayout: UICollectionViewLayout())
-        mainview1.reloadData(TestDate())
-        self.mainScroll.addSubview(mainview1)
-        self.mainScroll.bringSubviewToFront(mainview1)
-        
-        let mainview2 = NSBundle.mainBundle().loadNibNamed("SleepCareCollectionView", owner: self, options: nil).last as! SleepCareCollectionView
-        mainview2.frame = CGRectMake(1024, 0, 1024, self.mainScroll.frame.size.height)
-        //self.mainview = SleepCareCollectionView(frame: CGRectMake(0, 0, 300, 400), collectionViewLayout: UICollectionViewLayout())
-        mainview2.reloadData()
-        self.mainScroll.addSubview(mainview2)
-        self.mainScroll.bringSubviewToFront(mainview2)
         
         //去掉搜索按钮背景
-         //self.search.backgroundColor = UIColor(patternImage: UIImage(named:"transbg.png")!)
+        //self.search.backgroundColor = UIColor(patternImage: UIImage(named:"transbg.png")!)
         for(var i = 0 ; i < self.search.subviews[0].subviews.count; i++) {
             println(self.search.subviews[0].subviews[i])
             if(self.search.subviews[0].subviews[i].isKindOfClass(NSClassFromString("UISearchBarBackground"))){
@@ -58,14 +65,22 @@ class SleepcareMainController: BaseViewController,UIScrollViewDelegate {
         //设置分页控件
         self.curPager.pageCount = 3
         self.curPager.go()
+        
+        rac_setting()
     }
-
+    
+    //属性绑定
+    func rac_setting(){
+        sleepcareMainViewModel = SleepcareMainViewModel()
+        RACObserve(self.sleepcareMainViewModel, "BedModelList") ~> RAC(self, "BedViews")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     func TestDate() -> Array<BedModel> {
         var data:Array<BedModel> = Array<BedModel>()
         var item1 = BedModel()
@@ -83,7 +98,7 @@ class SleepcareMainController: BaseViewController,UIScrollViewDelegate {
         item2.UserName = "李四"
         item2.RoomNumber = "1"
         item2.HR = "86"
-         item2.BedStatus = "2"
+        item2.BedStatus = "2"
         data.append(item2)
         
         var item3 = BedModel()
@@ -113,12 +128,12 @@ class SleepcareMainController: BaseViewController,UIScrollViewDelegate {
     }
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
