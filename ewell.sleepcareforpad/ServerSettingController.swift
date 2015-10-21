1af16c89a97733d9113ed46e175c5c25071bf7fb
zhaoyin <zhaoyin@zhaoyindeMacBook-Pro.local>
2015-10-21 15:27:00 +0800

    12321

--- ewell.sleepcareforpad/ServerSettingController.swift
+++ ewell.sleepcareforpad/ServerSettingController.swift
@@ -11,11 +11,56 @@ import UIKit
 
 class ServerSettingController:BaseViewController {
     
+    @IBOutlet weak var txtServerJid: UITextField!
+    
+    @IBOutlet weak var txtServerAddress: UITextField!
+    
+    @IBOutlet weak var txtServerPort: UITextField!
+    
+    @IBOutlet weak var txtServerLoginName: UITextField!
+    
+    @IBOutlet weak var txtServerPwd: UITextField!
+    
     //-----------界面事件定义----------------
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.txtServerJid.text = GetValueFromPlist("serverjid")
+        self.txtServerAddress.text = GetValueFromPlist("xmppserver")
+        self.txtServerPort.text = GetValueFromPlist("xmppport")
+        self.txtServerLoginName.text = GetValueFromPlist("xmppusername")
+        self.txtServerPwd.text = GetValueFromPlist("xmppuserpwd")
     }
     
+    @IBAction func btnBackClick(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
     
+    @IBAction func btnSave(sender: AnyObject) {
+        try {
+            ({
+                SetValueIntoPlist("serverjid",self.txtServerJid.text)
+                SetValueIntoPlist("xmppserver",self.txtServerAddress.text)
+                SetValueIntoPlist("xmppport",self.txtServerPort.text)
+                SetValueIntoPlist("xmppusername",self.txtServerLoginName.text)
+                SetValueIntoPlist("xmppuserpwd",self.txtServerPwd.text)
+                
+                var alert = UIAlertController(title: "提示", message: "服务端配置保存成功", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler:self.clickOK))
+                self.presentViewController(alert, animated: true, completion: nil)
+                },
+                catch: { ex in
+                    //异常处理
+                    handleException(ex,showDialog: true)
+                },
+                finally: {
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                }
+            )}
+    }
     
+    func clickOK(action:UIAlertAction!)
+    {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
