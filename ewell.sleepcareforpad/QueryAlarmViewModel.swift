17160d9d8f381a078799ba0dabb5e214d7f17be8
zhaoyin <zhaoyin@zhaoyindeMacBook-Pro.local>
2015-10-30 11:13:05 +0800

    123

---
+++ ewell.sleepcareforpad/QueryAlarmViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QueryAlarmViewMode.swift
+//  ewell.sleepcareforpad
+//
+//  Created by zhaoyin on 15/10/30.
+//  Copyright (c) 2015年 djg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QueryAlarmViewModel
+{
+    
+}
