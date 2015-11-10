7cd0326bb628c1840740befb60f9038ea3c5c06c
djg2015 <434914655@qq.com>
2015-11-10 17:58:04 +0800

    1

---
+++ ewell.sleepcareforpad/IMainFrameViewController.swift
@@ -0,0 +1,51 @@
+//
+//  IMainFrameViewController.swift
+//
+//
+//  Created by djg on 15/11/10.
+//
+//
+
+import UIKit
+
+class IMainFrameViewController: IBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func FirstBtnClick(){
+        
+    }
+    
+    
+    func SecondBtnClick(){
+        
+    }
+    
+    
+    func ThirdBtnClick(){
+        
+    }
+    
+    func FourthBtnClick(){
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
