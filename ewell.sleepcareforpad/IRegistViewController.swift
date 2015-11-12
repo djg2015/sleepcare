edd7fbb8573ab70b5cbd29006f9371487da50ad1
djg2015 <434914655@qq.com>
2015-11-12 13:05:25 +0800

    1

---
+++ ewell.sleepcareforpad/IRegistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IRegistViewController.swift
+//  
+//
+//  Created by djg on 15/11/12.
+//
+//
+
+import UIKit
+
+class IRegistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ewell.sleepcareforpad/IRegistViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  IRegistViewModel.swift
+//  ewell.sleepcareforpad
+//
+//  Created by djg on 15/11/12.
+//  Copyright (c) 2015年 djg. All rights reserved.
+//
+
+import Foundation
