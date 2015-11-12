f431c65db398f321e0c846e02959cfc3e5641685
djg2015 <434914655@qq.com>
2015-11-12 13:01:07 +0800

    111

--- ewell.sleepcareforpad/IBaseViewController.swift
+++ ewell.sleepcareforpad/IBaseViewController.swift
@@ -9,25 +9,25 @@
 import UIKit
 
 class IBaseViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-       super.viewDidLoad()
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
 }
