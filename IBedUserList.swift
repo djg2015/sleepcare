f91c74d61f8252a904980ae9d0cfed0244c54498
Qinyuan Liu <lillix@Qinyuans-MacBook-Air.local>
2015-12-25 14:02:21 +0800

    12.25

--- IBedUserList.swift
+++ IBedUserList.swift
@@ -19,19 +19,39 @@ class IBedUserList: BaseMessage {
         //构造XML文档
         var doc = DDXMLDocument(XMLString: bodyXMl, options:0, error:nil)
         var bedUserList = doc.nodesForXPath("//BedUserInfo", error:nil) as! [DDXMLElement]
+        
         for bedUser in bedUserList {
             var bedUserInfo = IBedUserInfo(messageSubject: MessageSubject.ParseXmlToSubject(subjectXml))
+            if bedUser.elementForName("BedUserCode") != nil{
             bedUserInfo.BedUserCode = bedUser.elementForName("BedUserCode").stringValue()
-            bedUserInfo.BedUserName = bedUser.elementForName("BedUserName").stringValue()
+            }
+            if bedUser.elementForName("BedUserName") != nil{
+             bedUserInfo.BedUserName = bedUser.elementForName("BedUserName").stringValue()
+            }
+            if bedUser.elementForName("PartCode") != nil{
             bedUserInfo.PartCode = bedUser.elementForName("PartCode").stringValue()
+            }
+            if bedUser.elementForName("PartName") != nil{
             bedUserInfo.PartName = bedUser.elementForName("PartName").stringValue()
+            }
+            if bedUser.elementForName("RoomCode") != nil{
             bedUserInfo.RoomCode = bedUser.elementForName("RoomCode").stringValue()
+            }
+            if bedUser.elementForName("RoomName") != nil{
             bedUserInfo.RoomName = bedUser.elementForName("RoomName").stringValue()
+            }
+            if bedUser.elementForName("BedCode") != nil{
             bedUserInfo.BedCode = bedUser.elementForName("BedCode").stringValue()
+            }
+            if bedUser.elementForName("BedNumber") != nil{
             bedUserInfo.BedNumber = bedUser.elementForName("BedNumber").stringValue()
+            }
+            if bedUser.elementForName("EquipmentID") != nil{
             bedUserInfo.EquipmentID = bedUser.elementForName("EquipmentID").stringValue()
+            }
             result.bedUserInfoList.append(bedUserInfo)
         }
+        
         return result
     }
 
