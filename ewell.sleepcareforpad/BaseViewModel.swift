e210daf17ab1995d8ecacbf50e17d4fee1f22a31
Qinyuan Liu <lillix@Qinyuans-MacBook-Air.local>
2016-04-14 14:48:22 +0800

    test2.1.0

--- ewell.sleepcareforpad/BaseViewModel.swift
+++ ewell.sleepcareforpad/BaseViewModel.swift
@@ -9,20 +9,5 @@
 import UIKit
 
 class BaseViewModel: NSObject {
-//    var controller:BaseViewController?
-//  //  var controllerForIphone:IBaseViewController?
-//
-//    //跳转界面
-//    func JumpPage(jumpedViewController:BaseViewController){
-//        if(controller != nil){
-//            self.controller!.presentViewController(jumpedViewController, animated: false, completion: nil)
-//        }
-//    }
-    
-//    func JumpPageForIpone(jumpedViewController:IBaseViewController){
-//       
-//        if(controllerForIphone != nil){
-//            self.controllerForIphone!.presentViewController(jumpedViewController, animated: true, completion: nil)
-//        }
-//    }
+
 }
